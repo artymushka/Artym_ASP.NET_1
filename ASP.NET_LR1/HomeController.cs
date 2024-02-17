0a916bba04a2959e796bde776135b9a589a54955
artymushka <vladartym1@gmail.com>
2024-02-17 20:55:19 +0200

    ASP.NET_1

---
+++ ASP.NET_LR1/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var companyInfo = new
+            {
+                Name = "Vlad's Company",
+                Location = "Mykolaiv",
+                YearFounded = 2003
+            };
+
+            var randomNumber = new Random().Next(101);
+
+            ViewBag.CompanyInfo = companyInfo;
+            ViewBag.RandomNumber = randomNumber;
+
+            return View();
+        }
+    }
+}
+
+
+
+
---
+++ ASP.NET_LR1/MyCompany.cs
@@ -0,0 +1,9 @@
+﻿namespace ASP.NET_LR1
+{
+    public class MyCompany
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int YearFounded { get; set; }
+    }
+}
